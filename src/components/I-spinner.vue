a868c1fa1d4b2bd102bb755d6a018d90c82a0552
Tigran <tigranmogrov@gmail.com>
2024-01-18 20:53:49 +0200

    feat: add spinner component

---
+++ src/components/I-spinner.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  isLoading: boolean;
+}>();
+</script>
+
+<template>
+  <div class="spinner" v-if="isLoading">
+    <p>Your data is loading...</p>
+    <div class="spinner__inner">
+      <span class="spinner__line"></span>
+      <span class="spinner__line"></span>
+      <span class="spinner__line"></span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
