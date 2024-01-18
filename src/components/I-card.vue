dedc73701b575a63f968a9741dea1ffd312e66f9
Tigran <tigranmogrov@gmail.com>
2024-01-18 20:31:35 +0200

    feat: add card component

---
+++ src/components/I-card.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { IPost } from '@/types';
+
+defineProps<{
+  post: IPost;
+}>();
+</script>
+
+<template>
+  <div class="card">
+    <img class="card__avatar" :src="post.avatar" :alt="post.first_name" />
+    <div class="card__body">
+      <button
+        @click="$emit('getPostInfo', { id: post.id, method: 'show-info' })"
+        class="card__title">
+        {{ post.first_name }} {{ post.last_name }}
+      </button>
+      <a class="card__email-link" :href="'mailto:' + post.email">{{
+        post.email
+      }}</a>
+      <button
+        @click="$emit('getPostInfo', { id: post.id, method: 'delete' })"
+        class="btn card__button">
+        Delete
+      </button>
+
+      <button
+        @click="$emit('getPostInfo', { id: post.id, method: 'update' })"
+        class="btn card__button">
+        Update
+      </button>
+    </div>
+  </div>
+</template>
