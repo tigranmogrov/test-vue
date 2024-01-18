021513c1b3a49763c9ea8ef740f906cf3a3abef7
Tigran <tigranmogrov@gmail.com>
2024-01-18 20:32:21 +0200

    feat: add search component

---
+++ src/components/I-search.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="search">
+    <label class="form-field">
+      <input
+        name="search"
+        class="form-input"
+        @input="$emit('update:modelValue', $event.target.value)"
+        placeholder="Search"
+        type="search" />
+    </label>
+  </div>
+</template>
