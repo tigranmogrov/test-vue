aae0998501f222f9d21883c9bad828a3bbf67e9f
Tigran <tigranmogrov@gmail.com>
2024-01-18 20:38:23 +0200

    feat: add sorting component

---
+++ src/components/I-sorting.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ISort } from '@/types';
+
+defineProps<{
+  options: ISort[];
+}>();
+</script>
+
+<template>
+  <div class="sorting">
+    <label class="form-field">
+      <select
+        name="sort"
+        class="form-select"
+        @change="$emit('update:modelValue', $event.target.value)">
+        <option disabled selected value="">Choose</option>
+        <option
+          v-for="option in options"
+          :key="option.value"
+          :value="option.value">
+          {{ option.name }}
+        </option>
+      </select>
+    </label>
+  </div>
+</template>
